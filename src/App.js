c8a32aae5800a08901ac13d2f595345ecb2af40c
Van Vo <hongvant2@gmail.com>
2026-01-15 16:24:18 +1300

    fontandicon

--- src/App.js
+++ src/App.js
@@ -4,20 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt aut magni vel maxime molestiae tenetur maiores iure vero iusto ipsum explicabo voluptatum earum, consectetur, tempora, autem quia temporibus pariatur ab nulla numquam sapiente eligendi voluptate! Ut hic repellendus culpa quisquam explicabo voluptas officiis ad minima et, architecto id ipsam, delectus, laborum illum repudiandae saepe a? Voluptatem, ullam? Nulla molestiae voluptas, tempora amet in rerum exercitationem explicabo, quisquam dignissimos veritatis, corrupti dolores culpa error veniam quibusdam incidunt id ab quam quia sit modi vitae perspiciatis. Cum harum omnis voluptates officia ipsam excepturi consequuntur eveniet neque, maiores repudiandae nihil, obcaecati illum esse.
     </div>
   );
 }
